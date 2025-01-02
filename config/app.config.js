20480c5791c168d15aecfb5032b85b82de693d77
Lamintan5 <tech@studio5ive.org>
2025-01-02 22:59:24 +0300

    app

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+const ONE_SIGNAL_CONFIG = {
+    APP_ID: "091a8f82-7499-4bcf-b3e8-1b9367d7450c",
+    API_KEY: "os_v2_app_beni7atutff47m7idojwpv2fbsmsiqtopygett4ntpvywlrve2ki52huzzsegqnogaiaewg6vwpxesfwfyhtw4b6zy2e4citg4f5zni",
+};
+
+module.exports = {
+    ONE_SIGNAL_CONFIG,
+    
+}
