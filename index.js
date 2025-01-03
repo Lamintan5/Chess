24d5d60f4779bdffbff1059643d73deef75270ed
Lamintan5 <tech@studio5ive.org>
2025-01-03 21:29:40 +0300

    var 1

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-
+const { ONE_SIGNAL_CONFIG } = require("./config/app.config.js")
+const pushNotificationService = require("./services/push-notification.services.js");
+const express = require("express");
+const http = require("http");
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const routes = require("./routes");
 
 
 // Middleware
